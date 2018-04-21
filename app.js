8b6cb6a24a779f8175e892bc14738e3abe9ccef1
YuhanL1n <yuhan.lin1994@gmail.com>
2018-04-20 23:44:03 -0600

    draw points on map

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+require('./routes')(app);
 
 
 app.listen(process.env.PORT || 3000, function () {
